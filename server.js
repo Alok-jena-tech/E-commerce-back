493f859c7d1e4fd5bba5435c922f8d2e41e9031f
alok kumar <alokjena3896@gmial.com>
2025-09-14 15:45:21 +0530

    add server

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use("/api/subscribe",subscribeRoutes)
 app.use("/api/admin/users",adminRotues);
 app.use("/api/admin/products",productAdminRoutes);
 app.use("/api/admin/orders/",orderAdminRoutes)
-// app.listen(PORT,()=>{
-//     console.log(`server is running on http://localhost:${PORT}`)
-// })
-module.exports=app;
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`)
+})
+// module.exports=app;
